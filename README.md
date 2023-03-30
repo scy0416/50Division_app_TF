c057f7eebb5deba4c89e4ae8b3c796691f66d0c1
송찬영 <scy0416@gmail.com>
2023-03-30 13:11:36 +0900

    config.py파일 생성 후 작성(내부 내용은 사용자에 따라 정의)

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 **pip**: 22.3.1  
 **jinja2**: 3.1.2  
 **Flask** : 2.2.3  
-**Werkzeug**: 2.2.3
+**Werkzeug**: 2.2.3  
+**Bootstrap**: 5.2.3
---
+++ public_service_employee_application/config.py
@@ -0,0 +1,9 @@
+import os
+
+# 데이터베이스접속 주소
+SQLALCHEMY_DATABASE_URI =''
+
+# SQLAlchemy의 이벤트를 처리하는 옵션
+SQLALCHEMY_TRACK_MODIFICATIONS =False
+
+SECRET_KEY = ''
