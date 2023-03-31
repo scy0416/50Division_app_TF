13efed23e6fe2f3cbad78d46356fdcd5ccaab4c6
송찬영 <scy0416@gmail.com>
2023-03-31 14:48:57 +0900

    관리자가 인사정보 관리 창에 들어가기 되면 일반 사용자(공무직원)들의 정보가 출력되지만 모든 정보가 출력되지는 않고 식별 가능한 만큼만 출력된다.

--- public_service_employee_application/views/admin_views.py
+++ public_service_employee_application/views/admin_views.py
@@ -1,14 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 from public_service_employee_application.views.auth_views import login_required_admin
+from public_service_employee_application.models import User
 
 # 블루프린트 객체 생성
 bp = Blueprint('admin', __name__, url_prefix='/admin')
 
 # 이 블루프린트의 최초 진입점
 @bp.route('/', methods=('GET', ))
-# 직원으로 로그인이 되었나 확인하는 부분
+# 관리자로 로그인이 되었나 확인하는 부분
 @login_required_admin
 def index():
     #return render_template('base.html')
     return render_template('user/admin_main.html')
+
+# 인사정보 관리창
+@bp.route('/pr/', methods=('GET', ))
+@login_required_admin
+def pr_information():
+    page = request.args.get('page', type=int, default=1) # 페이지
+    user_list = User.query.filter_by(role='USER')
+    user_list = user_list.paginate(page=page, per_page=10)
+    return render_template('user/user_list.html', user_list=user_list)
