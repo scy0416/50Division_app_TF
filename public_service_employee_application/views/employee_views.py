745accb903371c7f9c4f1456bfa9b0f93d226573
송찬영 <scy0416@gmail.com>
2023-04-20 11:00:21 +0900

    휴가 신청 기능 구현

--- public_service_employee_application/views/employee_views.py
+++ public_service_employee_application/views/employee_views.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, render_template, request, session, redirect, url_for
+from flask import Blueprint, render_template, request, session, redirect, url_for, g
 from sqlalchemy import and_
 from datetime import datetime
 
 from public_service_employee_application.views.auth_views import login_required_employee
 from public_service_employee_application import db
-from public_service_employee_application.models import Post, User, Comment, HR_change_request
+from public_service_employee_application.models import Post, User, Comment, HR_change_request, Vacation_request
 from public_service_employee_application.forms import writeForm, contentForm
 
 # 블루프린트 객체 생성
@@ -124,3 +124,31 @@ def require_edit_pr(user_id):
     db.session.commit()
 
     return redirect(url_for('employee.pr_information', user_id=user_id))
+
+# 휴가 신청
+@bp.route('/vacation/', methods=('GET', ))
+@login_required_employee
+def vacation():
+    page = request.args.get('page', type=int, default=1)
+    vacation_request_list = Vacation_request.query.filter_by(user_id=g.user.id).order_by(Vacation_request.request_date.desc())
+    vacation_request_list = vacation_request_list.paginate(page=page, per_page=5)
+    return render_template('user/vacation.html', page=page, vacation_request_list=vacation_request_list)
+
+# 휴가 신청 등록
+@bp.route('/vacation/', methods=('POST', ))
+@login_required_employee
+def create_vacation_request():
+    from_date = request.form.get('from_date')
+    to_date = request.form.get('to_date')
+    reason = request.form.get('reason')
+    vacation_request = Vacation_request(
+        user_id=g.user.id,
+        from_date=from_date,
+        to_date=to_date,
+        reason=reason,
+        state='WAITING',
+        request_date=datetime.now()
+    )
+    db.session.add(vacation_request)
+    db.session.commit()
+    return redirect(url_for('employee.vacation'))
