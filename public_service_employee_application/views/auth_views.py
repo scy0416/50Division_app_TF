6a90dc7cb287f80e8291b349f7bb38b9e1990dfa
송찬영 <scy0416@gmail.com>
2023-05-18 16:16:34 +0900

    정상적으로 작동

--- public_service_employee_application/views/auth_views.py
+++ public_service_employee_application/views/auth_views.py
@@ -1,80 +1,70 @@
-import functools
-from flask import Blueprint, request, render_template, session, url_for, flash, g
-from werkzeug.utils import redirect
+from flask import Blueprint, render_template, redirect, url_for, request, flash, session, g
 from datetime import datetime
+import functools
 
-from public_service_employee_application.forms import UserLoginForm, joinForm
 from public_service_employee_application.models import User, Join_request
 from public_service_employee_application import db
+
 
 # 블루프린트 객체 생성
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-# 로그인 화면에 대한 요청으로 GET메소드는 로그인 화면 요청, POST메소드는 로그인 처리 요청이다.
-@bp.route('/login/', methods=('GET', 'POST'))
+
+# 이 블루프린트의 최초 진입점
+@bp.route('/login', methods=['GET'])
+def index():
+    return render_template('auth/login.html')
+
+
+# 로그인 처리
+@bp.route('/login', methods=['POST'])
 def login():
-    # 유처 로그인 폼을 생성
-    form = UserLoginForm()
-    # 가입 신청 폼 생성
-    join_form = joinForm()
+    userid = request.form.get('id')
+    password = request.form.get('password')
 
-    # 로그인 화면을 요구하는 경우
-    if request.method == 'GET':
-        # auth/login.html을 출력
-        return render_template('auth/login.html', form=form)
-    # 로그인 처리를 요청하는 경우
-    elif request.method == 'POST' and form.validate_on_submit():
-        # 에러 초기화
-        error = None
-        # id를 가지고 데이터를 뽑아내는 과정
-        user = User.query.filter_by(userid=form.id.data).first()
-        # 해당 id를 가진 유저가 없는 경우
-        if not user:
-            error = "존재하지 않는 사용자입니다."
-        # 로그인한 유저가 관리자인 경우
-        # 이 부분 역시 정적으로 확인할 것이 아니라 데이터베이스로부터 값을 가져와서 확인해야 한다.
-        elif form.password.data == user.password and user.role == 'ADMIN':
-            # 세션 초기화
-            session.clear()
-            # 세션에 id정보 저장
-            session['user_id'] = user.id
-            # 세션에 어드민 정보 저장
-            session['isAdmin'] = True
-            # 메인으로 리디렉션
-            return redirect(url_for('main.index'))
-        # 로그인한 유저가 일반유저인 경우
-        # 입력한 id가 유저의 id와 같고, 비밀번호가 그냥 같은 경우
-        # 암호화는 자바스크립트에서 하도록 바꾸기로 한다.
-        elif form.password.data == user.password and user.role == 'USER':
-            session.clear()
-            session['user_id'] = user.id
-            session['isAdmin'] = False
-            return redirect(url_for('main.index'))
-        flash(error)
-    return render_template('auth/login.html', form=form)
+    user = User.query.filter(User.userid == userid).first()
 
-# 로그아웃을 처리하는 부분
-@bp.route('/logout')
+    if not user:
+        flash("존재하지 않는 사용자입니다.")
+        return redirect(url_for('auth.index'))
+    elif user.password == password and user.role == 'ADMIN':
+        session.clear()
+        session['user_id'] = user.id
+        session['isAdmin'] = True
+        return redirect(url_for('admin.index'))
+    elif user.password == password and user.role == 'USER':
+        session.clear()
+        session['user_id'] = user.id
+        session['isAdmin'] = False
+        return redirect(url_for('employee.index'))
+    flash("알 수 없는 오류가 발생했습니다")
+    return redirect(url_for('auth.index'))
+
+
+# 로그아웃
+@bp.route('/logout', methods=['GET'])
 def logout():
     # 로그인 되어있던 정보를 리셋시킨다.
     session.clear()
     return redirect(url_for('main.index'))
 
 
+# 가입 페이지
 @bp.route('/join', methods=['GET'])
-def get_join_page():
+def join_page():
     return render_template('auth/join.html')
 
 
+# 가입 신청
 @bp.route('/join', methods=['POST'])
 def request_join():
     name = request.form.get('name')
-    birth_date = request.form.get('birth_date')
-    id = request.form.get('id')
+    birth_date =  request.form.get('birth_date')
+    userid = request.form.get('id')
     password = request.form.get('password')
 
     join_request = Join_request(
-        userid=id,
+        userid=userid,
         password=password,
         name=name,
         birth_date=birth_date,
@@ -85,7 +75,7 @@ def request_join():
     db.session.add(join_request)
     db.session.commit()
 
-    return redirect(url_for('main.index'))
+    return redirect(url_for('auth.index'))
 
 
 # 요청이 처리되기 전에 실행되는 어노테이션으로 로그인 된 정보가 존재한다면 로그인 된 사용자의 정보를 g.user에 담고 없으면 None을 저장한다.
@@ -102,22 +92,23 @@ def load_logged_in_user():
         # g객체의 user에 유저 정보를 담는다.
         g.user = User.query.get(user_id)
 
+
 # 관리자인지 확인하는 래퍼 메소드
 # 로그인 되어있지 않다면 로그인 화면으로 리디렉션 되고, 관리자가 아니라면 적절한 화면으로 리디렉션 된다.
-# TODO 무지성 리디렉션을 정리해야 한다.
 def login_required_admin(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         # g객체에 유저의 정보가 없다면
         if g.user is None:
-            # 메인화면으로 리디렉션 시킨다.
-            return redirect(url_for('main.index'))
+            # 로그인화면으로 리디렉션 시킨다.
+            return redirect(url_for('auth.index'))
         # 관리자가 아니라면
         elif g.user.role != 'ADMIN':
-            # 메인화면으로 리디렉션 시킨다.
-            return redirect(url_for('main.index'))
+            # 공무직원메인화면으로 리디렉션 시킨다.
+            return redirect(url_for('employee.index'))
         return view(**kwargs)
     return wrapped_view
+
 
 # 직원인지 확인하는 래퍼 메소드
 # 로그인 되어있지 않다면 로그인 화면으로 리디렉션 되고, 직원이 아니라면 적절한 화면으로 리디렉션 된다.
@@ -126,11 +117,11 @@ def login_required_employee(view):
     def wrapped_view(**kwargs):
         # g객체에 유저의 정보가 없다면
         if g.user is None:
-            # 메인화면으로 리디렉션 시킨다.
-            return redirect(url_for('main.index'))
+            # 로그인화면으로 리디렉션 시킨다.
+            return redirect(url_for('auth.index'))
         # 공무직원이 아니라면
         elif g.user.role != 'USER':
-            # 메인화면으로 리디렉션 시킨다.
-            return redirect(url_for('main.index'))
+            # 관리자 메인화면으로 리디렉션 시킨다.
+            return redirect(url_for('admin.index'))
         return view(**kwargs)
     return wrapped_view
--- public_service_employee_application/views/main_views.py
+++ public_service_employee_application/views/main_views.py
@@ -1,23 +1,23 @@
-from flask import Blueprint, url_for, session, request
-from werkzeug.utils import redirect
+from flask import Blueprint, session, redirect, url_for
 
-# 블루 프린트 객체 생성
+# 블루프린트 객체 생성
 bp = Blueprint('main', __name__, url_prefix='/')
 
+
 # 웹 페이지에 최초로 진입하는 부분
-# TODO 이 부분에서 다른 부분으로 리디렉션 시키는 것을 지향하기 위해 나중에는 각각 적절한 곳으로 리디렉션 할 수 있도록 바꾸도록 한다.
-@bp.route('/', methods=('GET', ))
+@bp.route('/', methods=['GET'])
 def index():
-    # GET메소드인 경우, 어드민인 경우
-    if request.method == 'GET' and session.get('isAdmin') == True:
-        # 어드민 메인화면으로 리디렉션
+    # 관리자인 경우
+    if session.get('isAdmin') == True:
+        # 관리자 메인화면으로 리디렉션
         return redirect(url_for('admin.index'))
-    # GET메소드인 경우, 어드민이 아니고, 로그인을 한 상황인 경우
-    elif (request.method == 'GET') and (session.get('isAdmin') == False) and (session.get('user_id') is not None):
-        # 일반직원 메인화면으로 리디렉션
+    # 공무직원인 경우
+    elif (session.get('isAdmin') is False) and (session.get('user_id') is not None):
+        # 공무직원 메인화면으로 리디렉션
         return redirect(url_for('employee.index'))
-    # GET메소드인 경우, 로그인하지 않은 경우
-    elif (request.method == 'GET') and (session.get('user_id') is None):
+    # 로그인 되어있지 않은 경우
+    elif session.get('user_id') is None:
         # 로그인 화면으로 리디렉션
-        return redirect(url_for('auth.login'))
-    #return redirect(url_for('auth.login'))
+        return redirect(url_for('auth.index'))
+    session.clear()
+    return redirect(url_for('main.index'))
